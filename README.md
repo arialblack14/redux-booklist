1ccdd1994494373af68d065b355f7f9dee80f791
arialblack14 <tsirasalexis@yahoo.gr>
2016-05-11 17:56:31 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Redux Book list app
+
+To run this just type
+
+```
+	> npm install
+	> npm start
+```
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: "Javascript: The good parts" },    
+        { title: "Harry Poter" },  
+        { title: "The Dark Tower" },  
+        { title: "Eloquent Ruby" }
+    ]
+}
